b40b1f801c5496b544ba5e7793d7106e9d556463
AbelTuoyintir <abeltuoyintir07@gamil.com>
2026-07-06 21:02:44 +0000

    connect

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -11,8 +11,8 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- Google Fonts -->
-    <link rel="icon" href="{{ asset('/images/wodabre-logo.png') }}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{ asset('/images/wodabre-logo.png') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('wodabre-logo.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('wodabre-logo.png') }}" type="image/x-icon">
 
     <!-- Lucida Font Stack -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
